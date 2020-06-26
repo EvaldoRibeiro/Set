170fbc78d4642917673ed975c0ac608bf6f5f02d
Evaldo Ribeiro <evardis@gmail.com>
2020-06-26 11:52:30 -0300

    Alterado Methodo LinkedHashSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
 		
 		set.add("Tv");
 		set.add("Tablet");
