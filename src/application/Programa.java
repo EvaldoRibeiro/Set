a45c8aea4feed89926b64499bf549f1b1a1dbf65
Evaldo Ribeiro <evardis@gmail.com>
2020-06-30 22:26:05 -0300

    Projeto Alterado2

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,7 +1,7 @@
 package application;
 
-
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.MyComparator;
@@ -10,16 +10,23 @@ import entities.Product;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.0));
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
-		
-		list.sort(new MyComparator());
-		
-		for (Product p: list) {
+
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+
+		list.sort(comp);
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
 
